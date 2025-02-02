e758a53eed2df8bc17ffdc0b00ed382168aab440
Alex M <alexmdev04@gmail.com>
2025-02-02 06:26:57 +0000

    Add EnemyBaker class

--- Assets/Scripts/Mono/EnemyMono.cs
+++ Assets/Scripts/Mono/EnemyMono.cs
@@ -1,7 +1,14 @@
+using Unity.Entities;
 using UnityEngine;
 
-namespace Metal.AuthoringAndMono {
+namespace Metal.Mono {
     public class EnemyMono : MonoBehaviour {
         
+    }
+    
+    public class EnemyBaker : Baker<EnemyMono> {
+        public override void Bake(EnemyMono authoring) {
+            //AddComponent(new ExampleComponent { });
+        }
     }
 }
