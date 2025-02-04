32ac0cb82f75d923e5da5ee8b25f69cd3204bdee
Alex M <alexmdev04@gmail.com>
2025-02-04 07:33:38 +0000

    Separate component tags into a single file

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,7 @@
+using Unity.Entities;
+
+namespace Metal.Tags { 
+    public struct Player : IComponentData { }
+    public struct Root : IComponentData { }
+    public struct PlayerControlledMovement : IComponentData { }
+}
