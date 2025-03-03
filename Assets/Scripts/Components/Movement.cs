2baa25996a6cda87c1d898e46603af1e23266deb
Alex M <alexmdev04@gmail.com>
2025-03-03 20:25:13 +0000

    Rename movement component direction to input

--- Assets/Scripts/Components/Movement.cs
+++ Assets/Scripts/Components/Movement.cs
@@ -6,7 +6,7 @@ namespace Metal.Components {
     /// Receives values from the controller system to be used by the movement system, the entity must have a tag defining what it's controlled by (e.g. Tags.PathedControlled).
     /// </summary>
     public struct Movement : IComponentData {
-        public float3 direction;
+        public float3 input;
         public float speed;
         public float speedMax;
     }
