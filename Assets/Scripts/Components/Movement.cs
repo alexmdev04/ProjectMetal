04c605e8890c1e992844cee8040561600ddba222
Alex M <alexmdev04@gmail.com>
2025-03-07 21:36:28 +0000

    Remove unused variables from movement component

--- Assets/Scripts/Components/Movement.cs
+++ Assets/Scripts/Components/Movement.cs
@@ -1,13 +1,13 @@
 using Unity.Entities;
 using Unity.Mathematics;
 
-namespace Metal.Components {
-    /// <summary>
-    /// Receives values from the controller system to be used by the movement system, the entity must have a tag defining what it's controlled by (e.g. Tags.PathedControlled).
-    /// </summary>
-    public struct Movement : IComponentData {
-        public float3 input;
-        public float speed;
-        public float speedMax;
+namespace Metal {
+    namespace Components {
+        /// <summary>
+        /// Receives values from the controller system to be used by the movement system, the entity must have a tag defining what it's controlled by (e.g. Tags.Controller.Pathed).
+        /// </summary>
+        public struct Movement : IComponentData {
+            public float3 input;
+        }
     }
 }
